31369edd962ec0ef15ef63e5ccffbb38d9acdc41
ryzen30xx <kiendtgch220563@fpt.edu.vn>
2025-01-16 20:04:35 +0700

    Change BE Project Type

--- be/obj/Debug/net9.0/be.AssemblyInfo.cs
+++ be/obj/Debug/net9.0/be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47a08f9e753e95af36facec7e8ff406fdc891fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0516f2dfadc0d0fca82542425702baeaaf944b")]
 [assembly: System.Reflection.AssemblyProductAttribute("be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
