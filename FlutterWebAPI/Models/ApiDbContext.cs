4c9ec0a3528f27d4c185526c4c4dfeb8aca9f942
Đinh Trung Kiên <110721139+ryzen30xx@users.noreply.github.com>
2025-02-21 22:48:15 -0800

    Add WebAPI Project

---
+++ FlutterWebAPI/Models/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace FlutterWebAPI.Models
+{
+    public class ApiDbContext :DbContext
+    {
+        public ApiDbContext(DbContextOptions option) : base(option)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ FlutterWebAPI/Models/users.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlutterWebAPI.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; } = "";
+        [Required]
+        public string Password { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+
+    }
+}
