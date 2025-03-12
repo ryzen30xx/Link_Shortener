43c921b15b8c26eea681dabd2a4a4b1e9049829e
ryzen30xx <kiendtgch220563@fpt.edu.vn>
2025-03-12 11:58:57 +0700

    Add Login Page

--- flutter/flutter_application_1/lib/views/home/home_view.dart
+++ flutter/flutter_application_1/lib/views/home/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/navigation_bar/navigation_bar.dart';
+import 'package:flutter_application_1/views/login/login_view.dart';
+import 'dart:html' as html;
 
 void main() {
   runApp(LinkShortenerApp());
@@ -8,7 +11,7 @@ class LinkShortenerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, 
       theme: ThemeData.dark(),
       home: HomeView(),
     );
@@ -18,16 +21,44 @@ class LinkShortenerApp extends StatelessWidget {
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var children = [
+      ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SignInPage()),
+          );
+        },
+        child: Text('Login'),
+      ),
+      SizedBox(width: 10),
+      ElevatedButton(
+        onPressed: () {},
+        child: Text('About'),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
-        title: Text('Link Shortener - The Original URL Shortener'),
-        backgroundColor: const Color.fromARGB(255, 3, 110, 255),
+        title: Text(
+          'Link Shortener - The Original URL Shortener',
+          style: TextStyle(color: Colors.black),
+        ),
+        titleSpacing: 20,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: children,
+            ),
+          ),
+        ],
       ),
-      backgroundColor: Colors.blue, // Set the background color to blue
+      backgroundColor: const Color.fromARGB(255, 39, 39, 39), // Set the background color to blue
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
+          child: Column( 
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -43,15 +74,14 @@ class HomeView extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Shorten URL'),
+              Opacity(
+                opacity: 1,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Shorten URL'),
+                ),
               ),
               SizedBox(height: 20),
-              Text(
-                'Want more out of your link shortener? Track link analytics, use branded domains, and manage your links with our paid plans.',
-                textAlign: TextAlign.center,
-              ),
             ],
           ),
         ),
--- flutter/flutter_application_1/lib/widgets/navigation_bar/navigation_bar.dart
+++ flutter/flutter_application_1/lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,43 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/login/login_view.dart';
 
-class NavigationBar extends StatelessWidget {
-  const NavigationBar({super.key});
-
+class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: Row(
-        children: <Widget>[
-          SizedBox(width: 60,
-          height: 180,
-          child: Image.asset('../../../web/icons/Icon-192.png'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Link Shortener - The Original URL Shortener',
+          style: TextStyle(color: Colors.black),
+        ),
+        titleSpacing: 20,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignInPage()),
+                    );
+                  },
+                  child: Text('Login'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('About'),
+                ),
+              ],
+            ),
           ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              _NavBarItem("Shorten Link"),
-              SizedBox(width: 60),
-              _NavBarItem('About'),
-              SizedBox(width: 60),
-              _NavBarItem('Login'),
-              SizedBox(width: 60),
-            ],
-          )
         ],
       ),
     );
-  }
-
-}
-
-class _NavBarItem extends StatelessWidget {
-  final String title;
-  const _NavBarItem(this.title);
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(title, style: TextStyle(fontSize: 18),);
   }
 }
