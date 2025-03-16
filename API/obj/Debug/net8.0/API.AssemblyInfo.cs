d2424f248dcf09f45154e1db5e34f6786eb17342
Đinh Trung Kiên <110721139+ryzen30xx@users.noreply.github.com>
2025-03-16 15:32:06 +0700

    Add Redis caching config in program.cs

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b440efcf2b50fb468bf02abd86a0ce81a5d1a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639a2d5fe38d71bfc0276173380b13b3872f5f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
